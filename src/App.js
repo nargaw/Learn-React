dcd5211300ae2aed3d07479ae1678ebaff80b57a
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-10-28 22:26:45 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from 'react'
+import Clicker from './Clicker.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App({ clickersCount, children })
+{
+    const [ hasClicker, setHasClicker ] = useState(true)
+    const [ count, setCount ] = useState(0)
+
+    const toggleClickerClick = () => 
+    {
+        setHasClicker(!hasClicker)
+    }
+
+    const increment = () => 
+    {
+        setCount(count + 1)
+    }
+
+    
+
+    const colors = useMemo(() => {
+        const colors = []
+
+        for(let i = 0; i < clickersCount; i++)
+        {
+            colors.push(`hsl(${Math.random() * 360}deg, 100%, 70%)`)
+        }
+
+        return colors
+    }, [ clickersCount ])
+
+    console.log(colors)
+   
+    return <>
+        {children}
+
+        <div>Total Count: {count} </div>
+
+        <button onClick={toggleClickerClick}>{ hasClicker ? 'Hide' : 'Show' } Clicker</button>
+        { hasClicker && <>
+            {[...Array(clickersCount)].map((value, index) =>
+                <Clicker
+                    key={ index } 
+                    increment = { increment } 
+                    keyName= { `count${index}` } 
+                    color={ colors[index] }
+                />
+            )}
+        </> }
+    </>
 }
-
-export default App;
