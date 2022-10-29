a575a6abf376e614965ffbe8ef503ab39eb08606
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-10-29 00:10:21 -0400

    update people fetch from API

--- src/People.js
+++ src/People.js
@@ -1,16 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function People()
 {
 
-    const [ people, setPeople ] = useState([
-        { id: 1, name: 'Jack'},
-        { id: 2, name: 'Jill'},
-        { id: 3, name: 'Jacob'},
-        { id: 4, name: 'Joe'}
-    ])
+    const [ people, setPeople ] = useState([])
 
-    console.log(people)
+    const getPeople = async () =>
+    {
+        //     const request = window.fetch('https://jsonplaceholder.typicode.com/users')
+        //     request.then((response) => {
+        //         const parse = response.json()
+                
+        //         parse.then((result) => {
+        //             console.log(result)
+        //         })
+        //    })
+
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then((response) => 
+        //     {
+        //         return response.json()
+        //     }) 
+        //     .then((result) =>
+        //     {
+        //         console.log(result)
+        //     })  
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const result = await response.json()
+        setPeople(result)
+            //.then(response => response.json())
+            //.then(result => console.log(result))
+    }
+
+    useEffect(() => 
+    {
+        getPeople()
+    }, [])
 
     return <div>
         <h2>People</h2>
